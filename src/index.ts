84cadae53c296121d428eb7b8222a065db4247f7
Tuomas Salmi <salmi.tuomas@gmail.com>
2022-04-10 22:34:56 +0300

    Add utility API to make authenticated requests easier to use
    Returns an object which acts as a client which remembers the
    token and sessionId so user doesn't have to pass those for each
    method.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
+import { isLoggedIn, login } from './auth/auth';
+import { getTeamMemberStatistics } from './statistics/statistics';
 export { login, isLoggedIn } from './auth/auth';
 export { getTeamStatistics, getTeamMemberStatistics } from './statistics/statistics';
+
+/**
+ * An utility API to let user to give login credentials only once and then call methods requiring
+ * authentication without credentials.
+ * @param username
+ * @param password
+ */
+export async function loggedInClient(username: string, password: string) {
+  const credentials = await login(username, password);
+
+  return {
+    getTeamMemberStatistics: (teamSlug: string, competitionSlug: string) =>
+      getTeamMemberStatistics(teamSlug, competitionSlug, credentials),
+    isLoggedIn: () => isLoggedIn(credentials),
+  };
+}
