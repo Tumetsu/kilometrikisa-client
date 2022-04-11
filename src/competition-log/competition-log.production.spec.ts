cd6304af6f558b064ae6a2f3317bb2baf3c1ebaf
Tuomas Salmi <salmi.tuomas@gmail.com>
2022-04-11 17:08:45 +0300

    Move competition log related methods to own file
    There will be more later so better have everything related to logs
    in one place.

---
+++ src/competition-log/competition-log.production.spec.ts
@@ -0,0 +1,19 @@
+import { login } from '../index';
+import { LoginCredentials } from '../auth/auth';
+import { getUserLogEntries } from './competition-log';
+
+describe('competition log', () => {
+  const username = process?.env['KILOMETRIKISA_USERNAME'] ?? '';
+  const password = process?.env['KILOMETRIKISA_PASSWORD'] ?? '';
+
+  let credentials: LoginCredentials;
+
+  beforeAll(async () => {
+    credentials = await login(username, password);
+  });
+
+  it("should fetch user's distance entries", async () => {
+    const results = await getUserLogEntries('45', 2021, credentials);
+    expect(results.length).toBeGreaterThan(0);
+  });
+});
