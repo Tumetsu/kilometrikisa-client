5b7d116ca96b98ad0e8e49de4c50b0c77c34cc97
Tuomas Salmi <salmi.tuomas@gmail.com>
2022-04-15 10:14:52 +0300

    Move url constants to a single place

--- src/statistics/statistics.ts
+++ src/statistics/statistics.ts
@@ -4,9 +4,7 @@ import { parseKilometrikisaTeamPageStatistics } from './html-parser/team-parser/
 import { parseKilometrikisaTeamMemberStatistics } from './html-parser/team-member-parser/team-member-parser';
 import { KilometrikisaError, KilometrikisaErrorCode } from '../utils/error-handling';
 import { axiosAuthGuard, getAuthConfig } from '../utils/requests';
-
-const kilometrikisaBaseUrl = 'https://www.kilometrikisa.fi';
-const kilometrikisaTeamPageBaseUrl = `${kilometrikisaBaseUrl}/teams/`;
+import { TEAM_PAGE_URL } from '../utils/urls';
 
 /**
  * Fetch team statistics from Kilometrikisa. Does not need authentication. Returns all statistic tables
@@ -17,7 +15,7 @@ const kilometrikisaTeamPageBaseUrl = `${kilometrikisaBaseUrl}/teams/`;
  */
 export async function getTeamStatistics(teamSlug: string) {
   try {
-    const teamStatisticsPageResponse = await axios.get(kilometrikisaTeamPageBaseUrl + teamSlug);
+    const teamStatisticsPageResponse = await axios.get(TEAM_PAGE_URL + teamSlug);
     return parseKilometrikisaTeamPageStatistics(teamStatisticsPageResponse.data);
   } catch (err) {
     throw new KilometrikisaError(
@@ -38,7 +36,7 @@ export async function getTeamMemberStatistics(
   contestSlug: string,
   credentials: SessionCredentials
 ) {
-  const url = `${kilometrikisaTeamPageBaseUrl}${teamSlug}/${contestSlug}/`;
+  const url = `${TEAM_PAGE_URL}${teamSlug}/${contestSlug}/`;
   try {
     const teamMemberStatisticsPage = await axiosAuthGuard(
       axios.get(url, getAuthConfig(url, credentials))
---
+++ src/utils/urls.ts
@@ -0,0 +1,10 @@
+export const KILOMETRIKISA_BASE_URL = 'https://www.kilometrikisa.fi';
+
+export const KILOMETRIKISA_LOGIN_URL = `${KILOMETRIKISA_BASE_URL}/accounts/login/`;
+export const KILOMETRIKISA_ACCOUNT_URL = `${KILOMETRIKISA_BASE_URL}/accounts/index/`;
+
+export const CONTEST_BASE_URL = `${KILOMETRIKISA_BASE_URL}/contests/`;
+export const CONTEST_LOG_LIST_URL = `${KILOMETRIKISA_BASE_URL}/contest/log_list_json/`;
+export const CONTEST_LOG_SAVE_URL = `${KILOMETRIKISA_BASE_URL}/contest/log-save/`;
+
+export const TEAM_PAGE_URL = `${KILOMETRIKISA_BASE_URL}/teams/`;
